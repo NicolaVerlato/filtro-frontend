903b8a68387a1f2e713fff39c3e2b427576074d3
Nicola Verlato <nicola.verlato00@gmail.com>
2023-06-08 16:29:38 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+jQuery(function($){
+    // pagina pubblicazioni
+    let press = $(".press");
+    let brochures = $(".brochures");
+
+    $("#press").click(function (){
+        $(this).css("background-color", "#1a6c7a");
+        $("#brochures").css("background-color", "#26ac8d");
+        $("#tutti").css("background-color", "#26ac8d");
+        brochures.each(function() {
+            $( this ).addClass( "none" );
+        });
+        press.each(function() {
+            if($( this ).hasClass( "none" ))
+            $( this ).removeClass( "none" );
+        });
+    })
+    $("#brochures").click(function (){
+        $(this).css("background-color", "#1a6c7a");
+        $("#press").css("background-color", "#26ac8d");
+        $("#tutti").css("background-color", "#26ac8d");
+        press.each(function() {
+            $( this ).addClass( "none" );
+        });
+        brochures.each(function() {
+            if($( this ).hasClass( "none" ))
+            $( this ).removeClass( "none" );
+        });
+    })
+    $("#tutti").click(function (){
+        $(this).css("background-color", "#1a6c7a");
+        $("#press").css("background-color", "#26ac8d");
+        $("#brochures").css("background-color", "#26ac8d");
+        press.each(function() {
+            if($( this ).hasClass( "none" ))
+            $( this ).removeClass( "none" );
+        });
+        brochures.each(function() {
+            if($( this ).hasClass( "none" ))
+            $( this ).removeClass( "none" );
+        });
+    })
+});
