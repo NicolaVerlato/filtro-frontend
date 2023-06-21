3fcf6efe11404fa0a0377fd63e21e65e6e9bc93c
NicolaVerlato <nicola.verlato00@gmail.com>
2023-06-21 18:01:07 +0200

    done

--- script.js
+++ script.js
@@ -1,53 +1,44 @@
 jQuery(function($){
     // pagina pubblicazioni
-    let press = $(".press");
-    let brochures = $(".brochures");
+    let btnPress = $("#press");
+    let btnBrochures = $("#brochures");
+    let btnTutti = $("#tutti");
 
-    $("#press").click(function (){
-        $(this).css("background-color", "#1a6c7a");
-        $("#brochures").css("background-color", "#26ac8d");
-        $("#tutti").css("background-color", "#26ac8d");
-        brochures.each(function() {
-            $( this ).removeClass( "remove-none" );
-            $( this ).addClass( "none" );
-        });
-        press.each(function() {
-            if($( this ).hasClass( "none" )){
-                $( this ).addClass( "remove-none" );
-                $( this ).removeClass( "none" );
+    let allBoxes = $('.container div');
+
+    let allBtn = [btnPress, btnBrochures, btnTutti];
+
+    allBtn.forEach(btn => {
+        btn.click(function (){
+            let thisBtn = this;
+            allBtn.forEach(btn => {
+                btn[0].classList.remove("active");
+            })
+            thisBtn.classList.add("active");
+
+            let thisBtnToLowerCase = thisBtn.outerText.toLowerCase(); 
+            for (let i = 0; i < allBoxes.length; i++) {
+                const element = allBoxes[i];
+                if(element.classList.contains(thisBtnToLowerCase)){
+                    removeNone(element)  
+                } else if(thisBtnToLowerCase == 'tutti'){
+                    removeNone(element)
+                } else{
+                    addNone(element)
+                }
             }
-        });
-    })
-    $("#brochures").click(function (){
-        $(this).css("background-color", "#1a6c7a");
-        $("#press").css("background-color", "#26ac8d");
-        $("#tutti").css("background-color", "#26ac8d");
-        press.each(function() {
-            $( this ).removeClass( "remove-none" );
-            $( this ).addClass( "none" );
-        });
-        brochures.each(function() {
-            if($( this ).hasClass( "none" )){
-                $( this ).addClass( "remove-none" );
-                $( this ).removeClass( "none" );
-            }
-        });
-    })
-    $("#tutti").click(function (){
-        $(this).css("background-color", "#1a6c7a");
-        $("#press").css("background-color", "#26ac8d");
-        $("#brochures").css("background-color", "#26ac8d");
-        press.each(function() {
-            if($( this ).hasClass( "none" )){
-                $( this ).addClass( "remove-none" );
-                $( this ).removeClass( "none" );
-            }
-        });
-        brochures.each(function() {
-            if($( this ).hasClass( "none" )){
-                $( this ).addClass( "remove-none" );
-                $( this ).removeClass( "none" );
-            }
-        });
-    })
+        })
+    });
 });
+
+function removeNone(item){
+    if(item.classList.contains( "none" )){
+        item.classList.add( "remove-none" );
+        item.classList.remove( "none" );
+    }
+}
+
+function addNone(item){
+    item.classList.remove( "remove-none" );
+    item.classList.add( "none" );
+}
